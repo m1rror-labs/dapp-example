ffe1d9cb04fdf4e6c9c858c9e9316eeb2eaf0f56
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2025-04-26 19:43:44 -0600

    fix example

--- tests/turbine-example.ts
+++ tests/turbine-example.ts
@@ -1,5 +1,5 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program, web3 } from "@coral-xyz/anchor";
 import { expect } from "chai";
 import { AnchorCounter } from "../target/types/anchor_counter";
 
@@ -10,27 +10,31 @@ describe("anchor-counter", () => {
 
   const program = anchor.workspace.AnchorCounter as Program<AnchorCounter>;
 
-  const counter = anchor.web3.Keypair.generate();
+  const signer = anchor.web3.Keypair.generate();
+  const counter = new web3.PublicKey(
+    "A6ttX3ak9Vp5qgUDSNUcbvYE3jT2G9yi6inFX7dQyQPP"
+  );
 
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods
       .initialize()
-      .accounts({ counter: counter.publicKey })
-      .signers([counter])
+      .accounts({ counter: counter })
+      .signers([signer])
       .rpc();
 
-    const account = await program.account.counter.fetch(counter.publicKey);
+    const account = await program.account.counter.fetch(counter);
     expect(account.count.toNumber()).to.equal(0);
   });
 
   it("Incremented the count", async () => {
     const tx = await program.methods
       .increment()
-      .accounts({ counter: counter.publicKey, user: provider.wallet.publicKey })
+      .accounts({ counter: counter, user: provider.wallet.publicKey })
       .rpc();
 
-    const account = await program.account.counter.fetch(counter.publicKey);
+    const account = await program.account.counter.fetch(counter);
+    console.log("account", counter.toBase58());
     expect(account.count.toNumber()).to.equal(1);
   });
 });
